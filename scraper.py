f4c205e50edef3fdee6d0827d92b37c3c623163f
Jiří Svoboda <george.freedom@centrum.cz>
2024-06-06 12:52:19 +0200

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -10,14 +10,22 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+# Constants
+URL = "https://www.notino.co.uk/toothpaste/"
+WAIT_TIMEOUT = 20
+PRODUCT_CONTAINER_SELECTOR = 'div[data-testid="product-container"]'
+PRICE_COMPONENT_SELECTOR = 'span[data-testid="price-component"]'
+DISCOUNT_SELECTOR = 'span.styled__DiscountValue-sc-1b3ggfp-1.jWXmOz'
+
+
 def get_element_text(element, by, value, default="NA"):
     """
-    Helper function to get the text of a web element
-    :param element: WebElement to search within
-    :param by: Method to locate elements
-    :param value: The locator value to be used with the 'by' parameter
-    :param default: The value to return if the element is not found (default is "NA")
-    :return: The text value of the element, or the default value if not found
+    Gets the text from the web element
+    :param element: web element
+    :param by: By object
+    :param value: tag name or CSS selector
+    :param default:
+    :return: text of the element
     """
     try:
         return element.find_element(by, value).text
@@ -27,13 +35,13 @@ def get_element_text(element, by, value, default="NA"):
 
 def get_element_attribute(element, by, value, attribute, default="NA"):
     """
-    Helper function to get the attribute of a web element
-    :param element: WebElement to search within
-    :param by: Method to locate elements
-    :param value: The locator value to be used with the 'by' parameter
-    :param attribute: The attribute name to retrieve from the found sub-element
-    :param default: The value to return if the sub-element or attribute is not found (default is "NA")
-    :return: The attribute value as a string, or the default value if not found
+    Gets the attribute from the web element
+   :param element: web element
+    :param by: By object
+    :param value: tag name or CSS selector
+    :param attribute: attribute name
+    :param default:
+    :return: attribute of the element
     """
     try:
         return element.find_element(by, value).get_attribute(attribute)
@@ -41,55 +49,63 @@ def get_element_attribute(element, by, value, attribute, default="NA"):
         return default
 
 
-# get url, AbstractScraper instance
-url = "https://www.notino.co.uk/toothpaste/"
-test = AbstractScraper()
-test.get_url(url)
-
-# wait for a page to load
-try:
-    WebDriverWait(test.driver, 20).until(
-        ec.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-testid="product-container"]'))
-    )
-except Exception as e:
-    logging.error(f"Error waiting for page to load: {e}")
-
-# all product containers
-all_product_containers = test.driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="product-container"]')
-
-# excluding PROMO banners
-product_containers = [container for container in all_product_containers
-                      if container.get_attribute("data-recommendation") != "promo-banner"]
-
-# check - total number of containers, excluding promo banners
-logging.info(f"Total number of all product containers: {len(all_product_containers)}")
-logging.info(f"Total number of products: {len(product_containers)}")
-
-
-for product_number, product_container in enumerate(product_containers, start=1):
-    # Extract data
+def extract_product_data(product_container):
+    """
+    Extracts the product data from the product container
+    :param product_container:
+    :return: dictionary of product data
+    """
     name = get_element_text(product_container, By.TAG_NAME, "h3")
     brand = get_element_text(product_container, By.TAG_NAME, "h2")
-    price = get_element_text(product_container, By.CSS_SELECTOR, 'span[data-testid="price-component"]')
+    price = get_element_text(product_container, By.CSS_SELECTOR, PRICE_COMPONENT_SELECTOR)
     link = get_element_attribute(product_container, By.CSS_SELECTOR, 'a', "href")
     image = get_element_attribute(product_container, By.CSS_SELECTOR, 'img', "src")
-    discount = get_element_text(product_container, By.CSS_SELECTOR, 'span.styled__DiscountValue-sc-1b3ggfp-1.jWXmOz',
-                                default="0")
+    discount = get_element_text(product_container, By.CSS_SELECTOR, DISCOUNT_SELECTOR, default="0")
+    return {
+        'name': name,
+        'brand': brand,
+        'price': price,
+        'link': link,
+        'image': image,
+        'discount': discount
+    }
 
-    logging.info(f"Number {product_number}, name: {name}")
-    logging.info(f"Number {product_number}, brand: {brand}")
-    logging.info(f"Number {product_number}, price: {price}")
-    logging.info(f"Number {product_number}, link: {link}")
-    logging.info(f"Number {product_number}, image: {image}")
-    logging.info(f"Number {product_number}, discount: {discount}")
 
-    # save data
-    test.save_data(name, brand, price, link, image, discount)
+def my_scraper(url):
+    """
+    Scrapes the given URL
+    :param url:
+    :return: None
+    """
+    scraper = AbstractScraper()
+    scraper.get_url(url)
 
-# end session, data exported to csv
-test.end_session()
-test.export_data_to_csv("notino_raw.csv")
+    # Wait for the page to load
+    try:
+        WebDriverWait(scraper.driver, WAIT_TIMEOUT).until(
+            ec.presence_of_all_elements_located((By.CSS_SELECTOR, PRODUCT_CONTAINER_SELECTOR))
+        )
+    except Exception as e:
+        logging.error(f"Error waiting for page to load: {e}")
 
-# add country and currency
-add_country_and_currency(url)
-transform_and_save_csv(url, "notino_raw.csv", "notino_transformed.csv")
+    all_product_containers = scraper.driver.find_elements(By.CSS_SELECTOR, PRODUCT_CONTAINER_SELECTOR)
+    product_containers = [container for container in all_product_containers
+                          if container.get_attribute("data-recommendation") != "promo-banner"]
+
+    logging.info(f"Total number of all product containers: {len(all_product_containers)}")
+    logging.info(f"Total number of products: {len(product_containers)}")
+
+    for product_number, product_container in enumerate(product_containers, start=1):
+        product_data = extract_product_data(product_container)
+        logging.info(f"Product {product_number}: {product_data}")
+        scraper.save_data(**product_data)
+
+    scraper.end_session()
+    scraper.export_data_to_csv("notino_raw.csv")
+
+    add_country_and_currency(url)
+    transform_and_save_csv(url, "notino_raw.csv", "notino_transformed.csv")
+
+
+if __name__ == "__main__":
+    my_scraper(URL)
